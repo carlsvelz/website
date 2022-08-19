f84355df88b2244abfb8b7c1ff5d24c0956cf708
nisarhassan12 <syednisarhassan12@gmail.com>
2022-08-19 17:00:10 +0500

    add some instructions comment.

--- src/lib/contents/docs/compatibility-matrix.ts
+++ src/lib/contents/docs/compatibility-matrix.ts
@@ -3,7 +3,7 @@ import type { Matrix } from "$lib/types/matrix.type";
 export const compatibilityMatrix: Matrix[] = [
   {
     name: "Orchestration System",
-    relevance: "admins",
+    relevance: "admins", // this is optional used for filtering based on relevance.
     components: [
       {
         name: "Kubernetes",
@@ -11,7 +11,7 @@ export const compatibilityMatrix: Matrix[] = [
         policy: {
           text: "the last minor three releases",
           description:
-            'The description can go here which can contain links as well <a href="/docs">Docs</a>.',
+            'The description can go here which can contain links as well <a href="/docs">Docs</a>.', // description it the tooltip that appears once the question mark is hovered upon.
         },
         supportedVersions: "1.22, 1.23 and 1.24",
       },
@@ -102,72 +102,7 @@ export const compatibilityMatrix: Matrix[] = [
     ],
   },
   {
-    name: "Orchestration System",
-    components: [
-      {
-        name: "Kubernetes",
-        availibility: "supported",
-        policy: {
-          text: "the last minor three releases",
-          description: null,
-        },
-        supportedVersions: "1.22, 1.23 and 1.24",
-      },
-      {
-        name: "Elastic Kubernetes Service (EKS)",
-        availibility: "not-supported",
-        policy: {
-          text: "default",
-          description: null,
-        },
-        supportedVersions: "default",
-      },
-      {
-        name: "Elastic Kubernetes Service (EKS)",
-        availibility: "supported",
-        policy: {
-          text: "default",
-          description: "",
-        },
-        supportedVersions: "default",
-      },
-    ],
-  },
-  {
     name: "Container Network Interface (CNI)",
-    relevance: "admins",
-    components: [
-      {
-        name: "Kubernetes",
-        availibility: "supported",
-        policy: {
-          text: "the last minor three releases",
-          description: null,
-        },
-        supportedVersions: "1.22, 1.23 and 1.24",
-      },
-      {
-        name: "Elastic Kubernetes Service (EKS)",
-        availibility: "not-supported",
-        policy: {
-          text: "default",
-          description: null,
-        },
-        supportedVersions: "default",
-      },
-      {
-        name: "Elastic Kubernetes Service (EKS)",
-        availibility: "supported",
-        policy: {
-          text: "default",
-          description: "",
-        },
-        supportedVersions: "default",
-      },
-    ],
-  },
-  {
-    name: "Image Registry",
     components: [
       {
         name: "Kubernetes",
